bbc31bdd0bd6f7e8f4a9f226d68d946c4538bd68
wunn <anh.nm00210@sinhvien.hoasen.edu.vn>
2023-09-11 17:06:05 +0700

    Updating death listener class
    This class cannot fully function

--- src/main/java/com/minefh/keepinventorybyslot/KeepInventoryBySlot.java
+++ src/main/java/com/minefh/keepinventorybyslot/KeepInventoryBySlot.java
@@ -1,17 +1,31 @@
 package com.minefh.keepinventorybyslot;
 
+import com.minefh.keepinventorybyslot.config.MainConfig;
+import com.minefh.keepinventorybyslot.listeners.PlayerDeathListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KeepInventoryBySlot extends JavaPlugin {
 
+    private MainConfig mainConfig;
+
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.mainConfig = new MainConfig();
     }
+
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // TODO: Implementing shutdown functions here
+    }
+
+
+    private void registerListeners() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+
+        pluginManager.registerEvents(new PlayerDeathListener(this.mainConfig), this);
     }
 }
--- src/main/java/com/minefh/keepinventorybyslot/listeners/PlayerDeathListener.java
+++ src/main/java/com/minefh/keepinventorybyslot/listeners/PlayerDeathListener.java
@@ -1,4 +1,32 @@
 package com.minefh.keepinventorybyslot.listeners;
 
-public class PlayerDeathListener {
+import com.minefh.keepinventorybyslot.config.MainConfig;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerDeathListener implements Listener {
+
+    private final MainConfig mainConfig;
+
+
+    public PlayerDeathListener(MainConfig mainConfig) {
+        this.mainConfig = mainConfig;
+    }
+
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        Player player = event.getPlayer();
+        Inventory playerInventory = player.getInventory();
+
+        for (ItemStack item: playerInventory.getContents()) {
+            if (item != null)
+                player.sendMessage(Component.text(item.getType().toString()));
+        }
+    }
 }
